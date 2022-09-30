5da7ac4d79141643b44f71d11638197ad8cd5a0c
AkashChakrabortty <akashchakrabortty@gmail.com>
2022-09-30 12:27:27 +0600

    'bee1143'

---
+++ beecrowd/1143(Squared and Cubic)/1143(Squared and Cubic).cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i,j,k,n;
+    cin >> n;
+
+    for(i=1;i<=n;i++){
+        for(j=i*i,k=i*j;j<i*i+1;j++){
+              cout << i << " " << j << " " << k <<endl;
+        }
+   }
+    return 0;
+}
