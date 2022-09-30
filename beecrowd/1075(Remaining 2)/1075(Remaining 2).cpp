3b943083b54e0e56f5cbdeea016a0f4365207f9b
AkashChakrabortty <akashchakrabortty@gmail.com>
2022-09-30 12:10:21 +0600

    'bee1075'

---
+++ beecrowd/1075(Remaining 2)/1075(Remaining 2).cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n,i = 2;
+    cin >> n;
+
+    while(i <= 10000){
+        cout << i << endl;
+        i+=n;
+    }
+
+    return 0;
+}
