338fc43d5bc513b50b565be8d612a2d41ce8b4ad
AkashChakrabortty <akashchakrabortty@gmail.com>
2022-09-30 12:18:47 +0600

    'bee1098'

---
+++ beecrowd/1098(Sequence IJ 4)/1098(Sequence IJ 4).cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    double i,j;
+
+   for(i=0;i<=2;i=i+0.2){
+        for(j=i+1;j<=i+3;j++){
+              cout << "I=" << i << " J=" << j <<endl;
+        }
+   }
+    return 0;
+}
