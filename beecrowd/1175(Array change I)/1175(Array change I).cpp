a69c3ce2e8c2a66a89189bb2508b403d0b9b7489
AkashChakrabortty <akashchakrabortty@gmail.com>
2023-01-16 12:04:24 +0600

    'bee1175'

---
+++ beecrowd/1175(Array change I)/1175(Array change I).cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n';
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+int main()
+{
+    optimize();
+    vector <int>x;
+    for(int i=0;i<20;i++){
+        int a;
+        cin>>a;
+        x.push_back(a);
+    }
+    reverse(x.begin(),x.end());
+    for( int j=0;j<20;j++){
+        cout << "N[" << j << "] = " << x[j] << endl;
+    }
+
+    return 0;
+}
