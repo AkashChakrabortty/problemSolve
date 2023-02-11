b13baafecf8d408a01998419259c035845e9726e
AkashChakrabortty <akashchakrabortty2000@gmail.com>
2023-02-11 14:08:51 +0600

    'bee1189'

---
+++ beecrowd/1189(Left Area)/1189(Left Area).cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n';
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+int main()
+{
+    optimize();
+    vector< vector<double>  > M;
+    string O;
+    cin>>O;
+    for(int i=0;i<12;i++){
+        vector<double>y;
+        for(int j=0;j<12;j++){
+            double a;
+            cin>> a;
+            y.push_back(a);
+        }
+        M.push_back(y);
+    }
+    double sum = 0;
+    double totalNumber = 0;
+    for(int i=1;i<11;i++){
+    for(int j=0;j<5;j++){
+            if(i < 6 && j < i){
+            sum += M[i][j] ;
+            totalNumber++;
+            }
+            else if(i>5 && i+j < 11){
+            sum += M[i][j] ;
+            totalNumber++;
+            }
+    }
+   }
+    double avg = sum/totalNumber;
+
+     if(O=="S")
+    {
+        cout << fixed << setprecision(1) << sum << endl;
+    }
+    else{
+        cout << fixed << setprecision(1) << avg << endl;
+    }
+    return 0;
+}
